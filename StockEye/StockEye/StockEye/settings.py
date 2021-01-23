28517cf76d051a1ef667a81319d827d9ec2ef7f0
Leron Bergelson <leronbergelson@gmail.com>
2021-01-22 19:38:01 -0500

    Updated Wesbite

--- StockEye/StockEye/StockEye/settings.py
+++ StockEye/StockEye/StockEye/settings.py
@@ -109,5 +109,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR,"static_files")
 STATIC_URL = '/static/'
-STATIC_ROOT = posixpath.join(*(BASE_DIR.split(os.path.sep) + ['static']))
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
