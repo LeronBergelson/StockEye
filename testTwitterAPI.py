61ecb537bcc49343f6bb16e3b6246e21e8f3faa8
Dys13xic <campbell.leckie@hotmail.com>
2021-03-17 20:23:20 -0700

    Read Twitter stream filter from stocks.txt

--- testTwitterAPI.py
+++ testTwitterAPI.py
@@ -16,9 +16,19 @@ try:
     APISecretKey = secretsFile.readline().strip()
     accessToken = secretsFile.readline().strip()
     acessTokenSecret = secretsFile.readline().strip()
+    secretsFile.close()
 except:
     print("ERROR: Unable to open/read secrets.txt")
     sys.exit(1)
+
+stocksFile = open("stocks.txt", "r")
+stocksFile.seek(0)
+stocks = []
+
+for line in stocksFile:
+    stocks.append("$" + line.strip())
+
+stocksFile.close()
 
 auth = tweepy.OAuthHandler(APIKey, APISecretKey)
 auth.set_access_token(accessToken, acessTokenSecret)
@@ -26,5 +36,4 @@ api = tweepy.API(auth)
 
 streamListener = MyStreamListener()
 stream = tweepy.Stream(auth = api.auth, listener=streamListener)
-
-stream.filter(languages=["en"], track=["$AMC", "$GME", "$TSLA"])
+stream.filter(languages=["en"], track=stocks)
