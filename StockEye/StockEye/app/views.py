945e7d053335dd63717e3dfd32e2d748560b2bda
Leron Bergelson <leronbergelson@gmail.com>
2021-04-06 10:55:43 -0400

    Merge Errors fixed

--- StockEye/StockEye/app/views.py
+++ StockEye/StockEye/app/views.py
@@ -6,6 +6,7 @@ from datetime import datetime
 from django.shortcuts import render
 from django.http import HttpRequest
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
 
 def home(request):
     """Renders the home page."""
@@ -55,3 +56,32 @@ def register(request):
 
     return render(request, 'app/registration.html', {'form':form})
 
+@login_required
+def watchlists(request):
+    """ Renders the watchlists page """
+    """
+    Alternative to @login_required decorator: manually test with:
+        request.user.is_authenticated
+    """
+    assert isinstance(request, HttpRequest)
+
+    # Get the current user
+    user = UserData.objects.filter(user=request.user).get()
+
+    # Get all of the user's watchlists
+    watchlists = user.stockResults.all()
+
+    # Additional data to pass to the templating engine
+    context = {
+        'title':'Watchlists',
+        'message':'Your Watchlist page.',
+        'year':datetime.now().year,
+        'user': request.user,
+        'watchlists': watchlists,
+    }
+
+    return render(
+        request,
+        'app/watchlists_test.html',
+        context
+    )
