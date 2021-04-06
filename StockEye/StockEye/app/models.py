6a1f528290641ee2d5c556e35bb042ed7971bd3d
Knifesurge <carlos94563@gmail.com>
2021-04-05 22:21:10 -0400

    Very basic watchlist functionality added to watchlists view

--- StockEye/StockEye/app/models.py
+++ StockEye/StockEye/app/models.py
@@ -7,7 +7,16 @@ from django.contrib.auth.models import User, Group
 class StockList(models.Model):
     stocks = models.CharField(max_length=60, blank=True, default = '')
 
+    def __str__(self):
+        return self.stocks
+
 class UserData(models.Model):
     user = models.OneToOneField(User, on_delete=models.PROTECT)
     stockResults = models.ManyToManyField('app.StockList')
+
+    class Meta:
+        ordering = ['user']
+
+    def __str__(self):
+        return f'{self.user}\n{self.stockResults}'
     
--- StockEye/StockEye/app/views.py
+++ StockEye/StockEye/app/views.py
@@ -3,9 +3,12 @@ Definition of views.
 """
 
 from datetime import datetime
+
 from django.shortcuts import render
 from django.http import HttpRequest
 from django.contrib.auth.decorators import login_required
+
+from .models import UserData
 
 def home(request):
     """Renders the home page."""
@@ -48,13 +51,29 @@ def about(request):
 @login_required
 def watchlists(request):
     """ Renders the watchlists page """
+    """
+    Alternative to @login_required decorator: manually test with:
+        request.user.is_authenticated
+    """
     assert isinstance(request, HttpRequest)
+
+    # Get the current user
+    user = UserData.objects.filter(user=request.user).get()
+
+    # Get all of the user's watchlists
+    watchlists = user.stockResults.all()
+
+    # Additional data to pass to the templating engine
+    context = {
+        'title':'Watchlists',
+        'message':'Your Watchlist page.',
+        'year':datetime.now().year,
+        'user': request.user,
+        'watchlists': watchlists,
+    }
+
     return render(
         request,
         'app/watchlists_test.html',
-        {
-            'title':'Watchlists',
-            'message':'Your Watchlist page.',
-            'year':datetime.now().year,
-        }
+        context
     )
