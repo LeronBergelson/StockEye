20f4bc8f46b197b33e31c8e7b296eafb0d498921
Knifesurge <carlos94563@gmail.com>
2021-04-07 11:37:57 -0400

    Add checks if user has no userdata in DB (possibly only currently superusers?)

--- StockEye/StockEye/app/views.py
+++ StockEye/StockEye/app/views.py
@@ -8,6 +8,8 @@ from django.http import HttpRequest
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 
+from .models import UserData
+
 def home(request):
     """Renders the home page."""
     assert isinstance(request, HttpRequest)
@@ -65,18 +67,27 @@ def watchlists(request):
     """
     assert isinstance(request, HttpRequest)
 
-    # Get the current user
-    user = UserData.objects.filter(user=request.user).get()
+    # Since these can throw Django errors if they don't exist, catch them
+    try:
 
-    # Get all of the user's watchlists
-    watchlists = user.stockResults.all()
+        # Get the current user
+        user = UserData.objects.filter(user=request.user).get()
 
+        # Get all of the user's watchlists
+        watchlists = user.stockResults.all()
+    except UserData.DoesNotExist:
+        # Unable to find a matching user, default to currently logged in 
+        # user and no watchlists
+        
+        user = request.user
+        watchlists = None
     # Additional data to pass to the templating engine
     context = {
         'title':'Watchlists',
         'message':'Your Watchlist page.',
         'year':datetime.now().year,
         'user': request.user,
+        'valid_user': True if watchlists is not None else False,
         'watchlists': watchlists,
     }
 
