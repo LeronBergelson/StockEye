70d32cd0d7d608d9091fb0df3bd00cf8de27710c
punkbass <77595024+punkbass@users.noreply.github.com>
2021-04-10 18:18:12 -0400

    Update MachineLearning.py

--- MachineLearning.py
+++ MachineLearning.py
@@ -101,9 +101,9 @@ def evaluate(tweet):
     #current_sentiment = Sentiment(result, getText(tweet), getID(tweet), getDate(tweet), getSymbol(tweet))
     
     if result == "Positive":
-        update = (positive_sentiment, positive_sentiment, getStock_ID(tweet))
+        update = (positive_sentiment, positive_sentiment, getStock_id(tweet))
     else:
-        update = (negative_sentiment, negative_sentiment, getStock_ID(tweet))
+        update = (negative_sentiment, negative_sentiment, getStock_id(tweet))
         
     current_connection = connection(r"StockEye\StockEye\db.sqlite3")
     
